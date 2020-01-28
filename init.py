6f5492bc5f6d7d33b13cf27356519f1e924af984
ivan780 <ivan780@hotmail.es>
2020-01-28 10:50:18 +0100

    Terminado script

--- init.py
+++ init.py
@@ -1,8 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://inshorts.com/en/read'
+# realizamos la peticion a la web
+url = 'https://computerhoy.com/'
 html = requests.get(url)
 
+# comprobamos que todo este bien por el codfigo de error
 if html.status_code == 200:
-    BeautifulSoup.find_all("div", class_="block-image")
+    soup = BeautifulSoup(html.content, 'html.parser')
+    div = soup.find_all("div", class_="block-title")
+    title = div[0].find('a').getText()
+
+    print(title)
